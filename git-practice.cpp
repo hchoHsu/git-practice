a05fe21e11b6d0a842871a4cabb8f01fe1640533
hchoHsu <vani900728@gmail.com>
2021-02-03 16:51:03 +0800

    add 2 dog file

--- git-practice.cpp
+++ git-practice.cpp
@@ -5,5 +5,8 @@ int main(){
     std::cout << "hello world!\n" << std::endl;
     std::cout << "new thing\n";
     std::cout << "WTF" << std::endl;
+    
+    for(int i = 0; i < 10; i++) cout << i << endl;
+    
     return 0;
 }

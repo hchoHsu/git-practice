42439b8c4bd3476ef998fdcfb59ccb05c5036cfc
hchoHsu <57358137+hchoHsu@users.noreply.github.com>
2021-02-07 23:03:47 +0800

    Update git-practice.cpp
    make a change on github to practice pull

--- git-practice.cpp
+++ git-practice.cpp
@@ -5,5 +5,8 @@ int main(){
     std::cout << "hello world!\n" << std::endl;
     std::cout << "new thing\n";
     std::cout << "WTF" << std::endl;
+    
+    
+    std::cout << "I change it diract from github!" << std::endl;
     return 0;
 }

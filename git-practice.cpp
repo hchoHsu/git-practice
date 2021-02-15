61a294e870219330484c879883e8fc5ba775e7b1
hchoHsu <vani900728@gmail.com>
2021-02-16 00:22:27 +0800

    add .exe file of git-practice.cpp

--- git-practice.cpp
+++ git-practice.cpp
@@ -10,5 +10,11 @@ int main(){
     std::cout << "I change it diract from github!" << std::endl;
     for(int i = 0; i < 10; i++) cout << i << endl;
     
+    #ifdef DEBUG
+    std::cout << "\nfor debug" << std::endl;
+    int j;
+    for(j = 0; j < 20; j++) cout << j << endl;
+    #endif
+
     return 0;
 }
